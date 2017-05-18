e3306b0efe45751388c108a82da1356f12ff372a
Jesse Ward <jward1990@gmail.com>
2017-05-18 17:46:37 -0400

    moved sockets to its own file

--- server/app.js
+++ server/app.js
@@ -1,21 +1,14 @@
 var express = require('express');
-var server = require('http');
-var io = require('socket.io');
 var app = express();
-server = server.Server(app);
-io = io(server);
-
-module.exports = io;
 var routes = require('./router.js');
 
 app.use(express.static('./app'));
 app.use('/_api', routes);
 
-server.listen(3000, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
 
-io.on('connection', function (socket) {
-  console.log("IO on");
-});
-
+module.exports = {
+  server: app
+};
--- server/sockets.js
+++ server/sockets.js
@@ -1,1 +1,9 @@
 var io = require('socket.io');
+var server = require('./app').server;
+io = io(server);
+
+io.on('connection', function (socket) {
+  console.log("IO on");
+});
+
+module.exports = io;
